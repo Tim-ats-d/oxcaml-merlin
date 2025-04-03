fca71933a7c2850fa33ab65a6baa74f3cc1619f7
Carine Morel <carine@tarides.com>
2025-04-03 13:49:07 +0200

    Better check to determine if the pipeline has already been shared.

--- src/kernel/mpipeline.ml
+++ src/kernel/mpipeline.ml
@@ -385,24 +385,26 @@ let process ?position ?state ?(pp_time = ref 0.0) ?(reader_time = ref 0.0)
   in
   let document_overrides =
       let { Ppx.parsetree; cache_version = ppx_cache_version; _ } = ppx
-       in
-       let { Document_overrides_with_cache.output; cache_was_hit; _ } =
-         Document_overrides_with_cache.apply
-           { ppx_parsetree = parsetree; ppx_cache_version }
-       in
-       document_overrides_cache_hit := cache_was_hit;
-       output
-  in
+        in
+        let { Document_overrides_with_cache.output; cache_was_hit; _ } =
+          Document_overrides_with_cache.apply
+            { ppx_parsetree = parsetree; ppx_cache_version }
+        in
+        document_overrides_cache_hit := cache_was_hit;
+        output
+    in
   let locate_overrides =
-      (let ({ Ppx.parsetree; cache_version = ppx_cache_version; _ }) = ppx
-       in
-       let { Locate_overrides_with_cache.output; cache_was_hit; _ } =
-         Locate_overrides_with_cache.apply
-           { ppx_parsetree = parsetree; ppx_cache_version }
-       in
-       locate_overrides_cache_hit := cache_was_hit;
-       output)
+    (let ({ Ppx.parsetree; cache_version = ppx_cache_version; _ }) = ppx
+      in
+      let { Locate_overrides_with_cache.output; cache_was_hit; _ } =
+        Locate_overrides_with_cache.apply
+          { ppx_parsetree = parsetree; ppx_cache_version }
+      in
+      locate_overrides_cache_hit := cache_was_hit;
+      output)
   in
+
+  let typer_has_been_shared = ref false in
   let typer =
     Effect.Deep.match_with (fun () ->
       timed typer_time (fun () ->
@@ -446,43 +448,37 @@ let process ?position ?state ?(pp_time = ref 0.0) ?(reader_time = ref 0.0)
                 }
               in
               Shared.locking_set shared.partial (Some mpipeline);
+              typer_has_been_shared := true;
               (* Back to [Mtyper.run] *)
               Effect.Deep.continue k ())
             | _ -> None }
   in
-  { config;
-    state;
-    raw_source;
-    source;
-    reader;
-    ppx;
-    typer;
-    pp_time;
-    reader_time;
-    ppx_time;
-    typer_time;
-    error_time;
-    ppx_cache_hit;
-    reader_cache_hit;
-    typer_cache_stats;
-    document_overrides;
-    document_overrides_cache_hit;
-    locate_overrides;
-    locate_overrides_cache_hit
-  }
-
-(* 
-Il faut faire :
-- calculer source, puis reader et ppx PUIS
-- une fonction qui calcul typeur et qui renvoie un résultat partiel quand dispo
-puis continue 
-
-- avec une callback (ou un effet -> Partial ) 
-- ou en interne
-
-Est-ce que l'utilisation d'un effet ne simplifie pas assez ?
-
-*)
+  if !typer_has_been_shared then
+    (* assert (Option.is_some position); *)
+    None
+  else
+    (* assert (position = None); *)
+    Some
+      { config;
+        state;
+        raw_source;
+        source;
+        reader;
+        ppx;
+        typer;
+        pp_time;
+        reader_time;
+        ppx_time;
+        typer_time;
+        error_time;
+        ppx_cache_hit;
+        reader_cache_hit;
+        typer_cache_stats;
+        document_overrides;
+        document_overrides_cache_hit;
+        locate_overrides;
+        locate_overrides_cache_hit
+      }
 
 let make ?position config source shared =
   process ?position (Mconfig.normalize config) source shared
@@ -581,10 +577,9 @@ let domain_typer shared () =
       | Some (config, source, potential_pos) ->
         Shared.set shared.config None;
         (try
-           let mpipeline = make ?position:potential_pos config source shared in
-           match potential_pos with
-           | None -> Shared.locking_set shared.partial (Some mpipeline)
-           | _ -> (* result already shared *) ()
+           match make ?position:potential_pos config source shared with
+           | Some _ as pipeline -> Shared.locking_set shared.partial pipeline
+           | None -> (* result already shared *) ()
          with
         | Domain_msg.Cancel_or_Closing -> ()
         | Mtyper.Exn_after_partial ->
